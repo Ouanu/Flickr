ebd149fc29ff81b18516ae782b6a44d74007feae
Ouanu <88440787+Ouanu@users.noreply.github.com>
2022-03-09 21:34:32 +0800

    Update PhotoInterceptor.java

--- app/src/main/java/com/moment/photogallery/api/PhotoInterceptor.java
+++ app/src/main/java/com/moment/photogallery/api/PhotoInterceptor.java
@@ -9,7 +9,7 @@ import okhttp3.Response;
 
 public class PhotoInterceptor implements Interceptor {
 
-    private static final String API_KEY = "9919616d9e0c55a370de8d1d1ef1ac64";
+    private static final String API_KEY = "YOUR_API";
 
     @Override
     public Response intercept(Chain chain) throws IOException {
